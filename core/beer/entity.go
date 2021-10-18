10eb3ba2f3f0329851b0f47f97b28e3a8944385a
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2021-10-18 00:56:21 -0300

    feat: web go structure

---
+++ core/beer/entity.go
@@ -0,0 +1,92 @@
+package beer
+
+// As tags json definem como os dados vão ser transformados em JSON
+type Beer struct {
+	ID    int64     `json:"id"`
+	Name  string    `json:"name"`
+	Type  BeerType  `json:"type"`
+	Style BeerStyle `json:"style"`
+}
+
+// https://www.thebeerstore.ca/beer-101/beer-types/
+type BeerType int
+
+const (
+	TypeAle   = 1
+	TypeLager = 2
+	TypeMalt  = 3
+	TypeStout = 4
+)
+
+// Desta forma a função String pertence ao tipo e pode ser usada da seguinte forma:
+// var x TypeAle
+// fmt.Println(x.String())
+func (t BeerType) String() string {
+	switch t {
+	case TypeAle:
+		return "Ale"
+	case TypeLager:
+		return "Lager"
+	case TypeMalt:
+		return "Malt"
+	case TypeStout:
+		return "Stout"
+	}
+	return "Unknown"
+}
+
+type BeerStyle int
+
+const (
+	StyleAmber = iota + 1 // definir automaticamente os ID's sequencialmente pelo compilador
+	StyleBlonde
+	StyleBrown
+	StyleCream
+	StyleDark
+	StylePale
+	StyleStrong
+	StyleWheat
+	StyleRed
+	StyleIPA
+	StyleLime
+	StylePilsner
+	StyleGolden
+	StyleFruit
+	StyleHoney
+)
+
+func (t BeerStyle) String() string {
+	switch t {
+	case StyleAmber:
+		return "Amber"
+	case StyleBlonde:
+		return "Blonde"
+	case StyleBrown:
+		return "Brown"
+	case StyleCream:
+		return "Cream"
+	case StyleDark:
+		return "Dark"
+	case StylePale:
+		return "Pale"
+	case StyleStrong:
+		return "Strong"
+	case StyleWheat:
+		return "Wheat"
+	case StyleRed:
+		return "Red"
+	case StyleIPA:
+		return "India Pale Ale"
+	case StyleLime:
+		return "Lime"
+	case StylePilsner:
+		return "Pilsner"
+	case StyleGolden:
+		return "Golden"
+	case StyleFruit:
+		return "Fruit"
+	case StyleHoney:
+		return "Honey"
+	}
+	return "Unknown"
+}
---
+++ web/main.go
@@ -0,0 +1,3 @@
+package web
+
+func main() {}
